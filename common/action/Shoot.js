d5f0e5900cc2336069f4fdb39ea7af5a24f93927
Frederic Theriault <fred.theriault@gmail.com>
2015-04-08 10:49:44 -0400

    Finished shoot skill

--- common/action/Shoot.js
+++ common/action/Shoot.js
@@ -1,12 +1,20 @@
 var Action = require('./Action');
 
-module.exports = Shoot = function (data) {
+module.exports = Shoot = function (data, level) {
 	if (data == null) {
-		Action.call(this, "shoot", 1000);
+		Action.call(this, "shoot", 2000, 700);
+
+		this.data.angle = 0;
+		this.data.speed = 0.9;
+		this.data.triggered = false;
 	}
 	else {
-		Action.call(this, "shoot", data.cooldown);
+		Action.call(this, "shoot", data.cooldown, 700);
 		this.data = data;
+	}
+
+	if (level != null) {
+		this.data.level = level;
 	}
 }
 
@@ -18,11 +26,49 @@ Shoot.prototype.getName = function () {
 }
 
 Shoot.prototype.update = function (fromSprite, delta) {
+	if (this.data.triggered && delta != null) {
 
+		this.moveProjectile(delta);
+
+		if (global != null && global.level != null) {
+			for (var i = 0; i < global.level.spriteList.length; i++) {
+				if (fromSprite.data.isPlayer != global.level.spriteList[i].data.isPlayer) {
+					var distance = Math.sqrt(Math.pow(this.data.x - global.level.spriteList[i].data.x, 2) + Math.pow(this.data.y - global.level.spriteList[i].data.y, 2));
+
+					if (distance < global.level.spriteList[i].data.minDistance) {
+						global.level.spriteList[i].hit(4 + this.data.level * 2, fromSprite);
+						
+						this.data.triggered = false;
+						fromSprite.broadcastState();
+					}
+				}
+			}
+		}
+		else {
+			game.ctx.beginPath();
+		    game.ctx.arc(this.data.x, this.data.y, 4, 0, 2 * Math.PI, false);
+			game.ctx.fillStyle = "brown";
+		    game.ctx.fill();
+		    game.ctx.closePath();
+		}
+
+		if (this.data.triggeredTime + 500 < (new Date()).getTime()) {
+			this.data.triggered = false;
+		}
+	}
 }
 
 Shoot.prototype.triggerEvent = function (fromSprite, mouseX, mouseY, toSprite) {
-	// trigger action and send animation
+	var success = false;
 
-	return true;
+	if (fromSprite.data.isPlayer != toSprite.data.isPlayer) {
+		success = true;
+		this.data.triggered = true;
+
+		this.data.angle = (Math.PI/180) * this.getAngle(fromSprite.data.x, fromSprite.data.y, toSprite.data.x, toSprite.data.y);
+		this.data.x = fromSprite.data.x;
+		this.data.y = fromSprite.data.y;
+	}
+
+	return success;
 }
